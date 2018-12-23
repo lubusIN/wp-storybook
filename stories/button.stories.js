e02f06ca8f3258c35556e20f4abe4615290b1435
Ajit Bohra <ajit@lubus.in>
2018-12-24 04:47:54 +0530

    Update ButtonGroup stories

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -4,6 +4,7 @@ import { storiesOf, addDecorator } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 import { withReadme, withDocs } from 'storybook-readme';
+import { withLiveEditScope } from 'storybook-addon-react-live-edit';
 
 import { Button } from "@wordpress/components";
 import ButtonReadme from '@wordpress/components/src/button/README.md'
@@ -21,15 +22,15 @@ const ButtonDecorator = (storyFn) => (
 
 storiesOf('Button', module)
   .addDecorator(ButtonDecorator)
-  .addDecorator(withReadme(ButtonReadme))
-  .add('Basic', () => (<Button onClick={action('clicked')}>Click Me</Button>))
-  .add('Href', () => <Button href="https://www.wordpress.org" onClick={action('clicked')}>Click Me</Button>)
-  .add('Default', () => <Button isDefault onClick={action('clicked')}>Click Me</Button>)
-  .add('Primary', () => <Button isPrimary onClick={action('clicked')}>Click Me</Button>)
-  .add('Large', () => <Button isLarge onClick={action('clicked')}>Click Me</Button>)
-  .add('Small', () => <Button isSmall onClick={action('clicked')}>Click Me</Button>)
-  .add('Toggled', () => <Button isToggled onClick={action('clicked')}>Click Me</Button>)
-  .add('Busy', () => <Button isPrimary isBusy onClick={action('clicked')}>Click Me</Button>)
-  .add('Link', () => <Button isLink onClick={action('clicked')}>Click Me</Button>)
-  .add('Focus', () => <Button focus onClick={action('clicked')}>Click Me</Button>);
-
+  .addDecorator(withReadme(ButtonReadme)) 
+  .addDecorator(withLiveEditScope({ React, Button, action }))
+  .addLiveSource('Basic', `return <Button onClick={action('clicked')}>Click Me</Button>`)
+  .addLiveSource('Href', `return <Button href="https://www.wordpress.org" onClick={action('clicked')}>Click Me</Button>`)
+  .addLiveSource('Default', `return <Button isDefault onClick={action('clicked')}>Click Me</Button>`)
+  .addLiveSource('Primary', `return <Button isPrimary onClick={action('clicked')}>Click Me</Button>`)
+  .addLiveSource('Large', `return <Button isLarge onClick={action('clicked')}>Click Me</Button>`)
+  .addLiveSource('Small', `return <Button isSmall onClick={action('clicked')}>Click Me</Button>`)
+  .addLiveSource('Toggled', `return <Button isToggled onClick={action('clicked')}>Click Me</Button>`)
+  .addLiveSource('Busy', `return <Button isPrimary isBusy onClick={action('clicked')}>Click Me</Button>`)
+  .addLiveSource('Link', `return <Button isLink onClick={action('clicked')}>Click Me</Button>`)
+  .addLiveSource('Focus', `return <Button focus onClick={action('clicked')}>Click Me</Button>`);
