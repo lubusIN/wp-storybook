d827470dab1060f1d5e7a11147355ba9fa65888c
Ajit Bohra <ajit@lubus.in>
2019-01-02 20:36:12 +0530

    Add state to RangeControl stories

--- stories/rangecontrol.stories.js
+++ stories/rangecontrol.stories.js
@@ -6,60 +6,81 @@ import { linkTo } from '@storybook/addon-links';
 import { withReadme, withDocs } from 'storybook-readme';
 
 import { RangeControl } from '@wordpress/components';
+import { withState } from '@wordpress/compose';
 import RangeControlReadme from '@wordpress/components/src/range-control/README.md';
+
+const RangeControlBasic =  withState( {
+  columns: 2,
+} )( ( { columns, setState } ) => ( 
+  <RangeControl
+  label="Columns"
+  value={ columns }
+  onChange={ ( columns ) => setState( { columns } ) }
+  min={ 2 }
+  max={ 10 }
+/>
+) );
+
+const RangeControlWithHelp =  withState( {
+  columns: 2,
+} )( ( { columns, setState } ) => ( 
+  <RangeControl
+  label="Columns"
+  help="Number of columns"
+  value={ columns }
+  onChange={ ( columns ) => setState( { columns } ) }
+  min={ 2 }
+  max={ 10 }
+/>
+) );
+
+const RangeControlWithBeforeIcon =  withState( {
+  columns: 2,
+} )( ( { columns, setState } ) => ( 
+  <RangeControl
+  label="Columns"
+  help="Number of columns"
+  beforeIcon="layout"
+  value={ columns }
+  onChange={ ( columns ) => setState( { columns } ) }
+  min={ 2 }
+  max={ 10 }
+/>
+) );
+
+const RangeControlWithAfterIcon =  withState( {
+  columns: 2,
+} )( ( { columns, setState } ) => ( 
+  <RangeControl
+  label="Columns"
+  help="Number of columns"
+  afterIcon="layout"
+  value={ columns }
+  onChange={ ( columns ) => setState( { columns } ) }
+  min={ 2 }
+  max={ 10 }
+/>
+) );
+
+const RangeControlWithReset =  withState( {
+  columns: 2,
+} )( ( { columns, setState } ) => ( 
+  <RangeControl
+  label="Columns"
+  help="Number of columns"
+  afterIcon="layout"
+  value={ columns }
+  onChange={ ( columns ) => setState( { columns } ) }
+  min={ 2 }
+  max={ 10 }
+  allowReset={ true }
+/>
+) );
 
 storiesOf('Components|RangeControl', module)
   .addDecorator(withReadme(RangeControlReadme))
-  .add('Basic', () => (
-    <RangeControl
-        label="Columns"
-        value={ 6 }
-        onChange={ action( 'changed' ) }
-        min={ 2 }
-        max={ 10 }
-    />
-  ))
-  .add('With Help', () => (
-    <RangeControl
-        label="Columns"
-        help="Number of columns"
-        value={ 6 }
-        onChange={ action( 'changed' ) }
-        min={ 2 }
-        max={ 10 }
-    />
-  ))
-  .add('With Before Icon', () => (
-    <RangeControl
-        label="Columns"
-        help="Number of columns"
-        beforeIcon="layout"
-        value={ 6 }
-        onChange={ action( 'changed' ) }
-        min={ 2 }
-        max={ 10 }
-    />
-  ))
-  .add('With After Icon', () => (
-    <RangeControl
-        label="Columns"
-        help="Number of columns"
-        afterIcon="layout"
-        value={ 6 }
-        onChange={ action( 'changed' ) }
-        min={ 2 }
-        max={ 10 }
-    />
-  ))
-  .add('With Reset', () => (
-    <RangeControl
-        label="Columns"
-        help="Number of columns"
-        afterIcon="layout"
-        value={ 6 }
-        onChange={ action( 'changed' ) }
-        min={ 2 }
-        max={ 10 }
-        allowReset={ true }
-    />
-  ));
+  .add('Basic', () => <RangeControlBasic />)
+  .add('With Help', () => <RangeControlWithHelp />)
+  .add('With Before Icon', () => <RangeControlWithBeforeIcon />)
+  .add('With After Icon', () => <RangeControlWithAfterIcon />)
+  .add('With Reset', () => <RangeControlWithReset />);
