169ff832d6207f347817cecd17bed682bf1a41ad
Ajit Bohra <ajit@lubus.in>
2019-09-18 05:30:47 +0530

    Upgrade to storybook v5.2

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,9 +1,2 @@
-/**
- * Register
- */
-import '@storybook/addon-options/register';
-import '@storybook/addon-links/register';
-import '@storybook/addon-viewport/register';
-import 'storybook-readme/register';
-import '@storybook/addon-a11y/register';
 import '@storybook/addon-actions/register';
+import '@storybook/addon-links/register';
