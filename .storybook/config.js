c7c9b9398587a61c367bebfff690b7ef027cb031
Ajit Bohra <ajit@lubus.in>
2019-09-20 02:55:12 +0530

    Setup options theme base

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,19 @@
-import { configure } from '@storybook/react';
+/**
+ * Storybook dependencies, addons and configurations
+ */
+import { configure, addParameters } from '@storybook/react';
+import { options } from './options';
+
 
 /**
- * WordPress Component Styles
+ * WordPress Components Style
  */
 import "@wordpress/components/build-style/style.css";
+
+/**
+ * Configuration
+ */
+addParameters({ options });
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../stories', true, /\.stories\.js$/), module);
